8635c6e9dccd9dc6f643129d618f59c56f983f2a
Sai Sashreek D <saisashreekd@gmail.com>
2025-06-04 13:05:29 +0530

    Updated Sidebar with New Icon and Content

--- app/_components/AppSidebar.tsx
+++ app/_components/AppSidebar.tsx
@@ -1,82 +1,92 @@
-import React from 'react'
+import React from "react";
 import {
-    Sidebar,
-    SidebarContent,
-    SidebarFooter,
-    SidebarGroup,
-    SidebarGroupContent,
-    SidebarGroupLabel,
-    SidebarHeader,
-    SidebarMenu,
-    SidebarMenuButton,
-    SidebarMenuItem,
-} from "@/components/ui/sidebar"
-import { Calendar, Home, Inbox, Search, Settings } from "lucide-react"
-import Image from 'next/image'
-import { usePathname } from 'next/navigation'
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
+import { Calendar, Inbox, Layers, UserCircle, Wallet } from "lucide-react";
+import Image from "next/image";
+import { usePathname } from "next/navigation";
 
 const items = [
-    {
-        title: "Home",
-        url: "#",
-        icon: Home,
-    },
-    {
-        title: "Inbox",
-        url: "#",
-        icon: Inbox,
-    },
-    {
-        title: "Calendar",
-        url: "#",
-        icon: Calendar,
-    },
-    {
-        title: "Search",
-        url: "#",
-        icon: Search,
-    },
-    {
-        title: "Settings",
-        url: "#",
-        icon: Settings,
-    },
-]
+  {
+    title: "Workspace",
+    url: "#",
+    icon: Layers,
+  },
+  {
+    title: "AI Tools",
+    url: "#",
+    icon: Inbox,
+  },
+  {
+    title: "My History",
+    url: "#",
+    icon: Calendar,
+  },
+  {
+    title: "Billing",
+    url: "#",
+    icon: Wallet,
+  },
+  {
+    title: "Profile",
+    url: "#",
+    icon: UserCircle,
+  },
+];
 
 export function AppSidebar() {
-    const path = usePathname();
-    return (
-        <Sidebar>
-            <SidebarHeader>
-                <div className='p-4'>
-                    <Image src={'./logo.svg'} alt='logo' width={100} height={100}
-                        className='w-full h-full' />
-                    <h2 className='text-sm text-gray-400 text-center'>Build Awesome</h2>
-                </div>
-            </SidebarHeader>
-            <SidebarContent>
-                <SidebarGroup>
-
-                    <SidebarGroupContent>
-                        <SidebarMenu className='mt-5'>
-                            {items.map((item, index) => (
-                                // <SidebarMenuItem key={item.title} className='p-2'>
-                                //     <SidebarMenuButton asChild className=''>
-                                <a href={item.url} key={index} className={`p-2 text-lg flex gap-2 items-center
-                                 hover:bg-gray-100 rounded-lg ${path.includes(item.url) && 'bg-gray-200ß'}`}>
-                                    <item.icon className='h-5 w-5' />
-                                    <span>{item.title}</span>
-                                </a>
-                                //     </SidebarMenuButton>
-                                // </SidebarMenuItem>
-                            ))}
-                        </SidebarMenu>
-                    </SidebarGroupContent>
-                </SidebarGroup>
-            </SidebarContent>
-            <SidebarFooter>
-                <h2 className='p-2 text-gray-400 text-sm'>Copyright @Tubeguruji</h2>
-            </SidebarFooter>
-        </Sidebar>
-    )
+  const path = usePathname();
+  return (
+    <Sidebar>
+      <SidebarHeader>
+        <div className="p-4">
+          <Image
+            src={"/logo.png"}
+            alt="logo"
+            width={100}
+            height={70}
+            className="w-full"
+          />
+          <h2 className="text-sm text-gray-400 text-center mt-3">Build Awesome Skills</h2>
+        </div>
+      </SidebarHeader>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupContent>
+            <SidebarMenu className="mt-2">
+              {items.map((item, index) => (
+                // <SidebarMenuItem key={item.title} className='p-2'>
+                //     <SidebarMenuButton asChild className=''>
+                <a
+                  href={item.url}
+                  key={index}
+                  className={`p-2 text-lg flex gap-2 items-center
+                                 hover:bg-gray-100 rounded-lg ${
+                                   path.includes(item.url) && "bg-gray-200ß"
+                                 }`}
+                >
+                  <item.icon className="h-5 w-5" />
+                  <span>{item.title}</span>
+                </a>
+                //     </SidebarMenuButton>
+                // </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+      <SidebarFooter>
+        <h2 className="p-2 text-gray-400 text-sm">Copyright @Graphiz</h2>
+      </SidebarFooter>
+    </Sidebar>
+  );
 }
